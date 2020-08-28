f97d48dbf4ba670b2cf8188af9764a2b81e9e2c1
Faviola Caballero <fabycg18@gmail.com>
2020-08-27 21:01:35 -0700

    added check marks and cross and changed color

--- js/quiz.js
+++ js/quiz.js
@@ -12,23 +12,55 @@ function checkResult() {
 //answe1
 if (answer1.checked == true) {
     add ++;
+        //&#10004 is the unicode for the checkmark & &#10006 is the unicode for the cross.
+    document.getElementById('chk1').innerHTML = "&#10004";
+    document.getElementById('chk1').style.color = "green";
+} else {
+    document.getElementById('chk1').innerHTML = "&#10006";
+    document.getElementById('chk1').style.color = "red";
 }
+
 //answer2
 if (answer2.checked == true) {
     add ++;
+    document.getElementById('chk2').innerHTML = "&#10004";
+    document.getElementById('chk2').style.color = "green";
+} else {
+    document.getElementById('chk2').innerHTML = "&#10006";
+    document.getElementById('chk2').style.color = "red";
 }
+
 //answer3
 if (answer3.checked == true) {
     add ++;
+    document.getElementById('chk3').innerHTML = "&#10004";
+    document.getElementById('chk3').style.color = "green";
+} else {
+    document.getElementById('chk3').innerHTML = "&#10006";
+    document.getElementById('chk3').style.color = "red";
+
 }
+
 //answer4
 if (answer4.checked == true) {
     add ++;
+    document.getElementById('chk4').innerHTML = "&#10004";
+    document.getElementById('chk4').style.color = "green";
+} else {
+    document.getElementById('chk4').innerHTML = "&#10006";
+    document.getElementById('chk4').style.color = "red";
 }
+
 //answer5
 if (answer5.checked == true) {
     add ++;
+    document.getElementById('chk5').innerHTML = "&#10004";
+    document.getElementById('chk5').style.color = "green";
+} else {
+    document.getElementById('chk5').innerHTML = "&#10006";
+    document.getElementById('chk5').style.color = "red";
 }
+
  document.getElementById("score").innerHTML = "Score:" +" "+ add;
 
 }
